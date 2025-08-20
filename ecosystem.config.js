92225d66947b6f27faa73411adc80394701b33cc
TambongStercy <stercytambong@gmail.com>
2025-08-20 04:04:17 +0100

    m2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   apps: [{
     name: 'ssic-sms-ui',
-    script: 'npm',
-    args: 'start',
+    script: './node_modules/.bin/next',
+    args: 'start -p 3060',
     cwd: '/var/www/SMS_Simbtech',
     instances: 1,
     autorestart: true,
